44a1541885c79799f1ccc4e8555de8f1a276d413
winslj <137501640+winslj@users.noreply.github.com>
2023-09-30 19:57:59 +0100

    Create deepCopy.js

---
+++ deepCopy.js
@@ -0,0 +1,36 @@
+const deepCopy = (sourceObj) => {
+    // Used a function I found online (Object.assign function)
+    const copyObj = Object.assign({}, sourceObj)
+    return copyObj
+};
+
+// Example simple test case
+const source = {
+    a: 1,
+    b: 'string',
+    c: false,
+};
+
+const target = deepCopy(source);
+console.group('Set1');
+console.log('source ==>', source);
+console.log('target ==>', target);
+console.groupEnd();
+
+// Example more advanced test case
+const source1 = {
+    a: [1, 2, 3, 4],
+    b: {
+        c: 1,
+        d: 2,
+        e: new Date(),
+        f: () => console.log('Hello World'),
+    },
+};
+const target1 = deepCopy(source1);
+console.group('Set2');
+console.log('source ==>', source1);
+console.log('target ===>', target1);
+console.groupEnd();
+
+// Feel free to show off different style test cases as you see fit
